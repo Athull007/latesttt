fbf1d6e04e5f4a8fe877c85eaa317c8f102526aa
abdulaslamka <abdulaslam2712@gmail.com>
2024-01-08 12:14:55 +0530

    latestCode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import Interface from "./components/Interface";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+
+
+import AdminLogin from "./components/AdminLogin";
+import Psignup from "./components/Psignup";
+import Plogin from "./components/Plogin";
+import DoctorList from './components/DoctorList';
+import EditDoctor from './components/EditDoctor';
+import Dashboard from "./components/Dashboard";
+import Interfac from "./components/Interfac";
+import Myappoinments from "./components/Myappoinments";
+import Sidebar from "./components/Sidebar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+ <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Interface/>}></Route>
+      <Route path='/Side' element={<Sidebar/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/patient login' element={<Plogin/>}></Route>
+      
+      <Route path='/register' element={<SignUp />}></Route>
+      <Route path='/patient register' element={<Psignup />}></Route>
+      <Route path='/admin' element={<AdminLogin />}></Route>
+      <Route path='/doctorview' element={<DoctorList />}></Route>
+      <Route path='/edit-doctor/:id' element={<EditDoctor />}></Route>
+      <Route path='/dash' element={<Dashboard/>}></Route>
+      <Route path='/admindashboard' element={<Interfac/>}></Route>
+      <Route path='/Myapp' element={<Myappoinments/>}></Route>
+
+    </Routes>
+    </BrowserRouter>
+
     </div>
   );
 }
